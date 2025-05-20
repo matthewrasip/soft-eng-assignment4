c45963c38d65b89005b51feec33826ffb91ce7bb
Nikhil Govula <s3900665@student.rmit.edu.au>
2025-05-20 13:31:57 +1000

    Added base code for Person.java with variables.

--- src/main/java/com/roadregistry/Person.java
+++ src/main/java/com/roadregistry/Person.java
@@ -1,4 +1,24 @@
 package com.roadregistry;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Person {
+    private String id;
+    private String name;
+    private int age;
+
+    public Person(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters and setters (used for tests- Matthew)
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+
+    public void setName(String name) { this.name = name; }
+    public void setAge(int age) { this.age = age; }
 }
